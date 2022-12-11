9929a1a1a920f59165a402fb0bcb512e5f2e04da
abhiraj2912 <abhirajaravind408@gmail.com>
2022-12-11 23:56:10 +0530

    services

---
+++ src/app/api.service.ts
@@ -0,0 +1,36 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  constructor(private http:HttpClient) { }
+
+  addEmployee=(employeeData:any)=>
+  {
+    return this.http.post("http://localhost:8080/addemployee",employeeData)
+  }
+  addTask=(taskData:any)=>
+  {
+    return this.http.post("http://localhost:8080/addtask",taskData)
+  }
+  login=(loginData:any)=>
+  {
+    return this.http.post("http://localhost:8080/login",loginData)
+  }
+
+  employeeTaskView=(employeeID:any)=>
+  {
+    return this.http.post("http://localhost:8080/empviewtask",employeeID)
+  }
+
+  adminTaskView=()=>
+  {
+    return this.http.get("http://localhost:8080/adminviewtask")
+  }
+
+  employeeTaskSearch=(employeename:any)=>
+  {return this.http.post("http://localhost:8080/adminsearchtask",employeename)}
+}
